6c90d6781bf9501d95faca47823304cd7d3dd93e
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2023-03-28 15:12:23 -0300

    aplicando react hook form

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -49,7 +49,12 @@ export const Content = styled(Dialog.Content)`
             border-radius: 6px;
             margin-top: 1.25rem;
             cursor: pointer;
-            &:hover {
+
+            &:disabled {
+                opacity: 0.6;
+                cursor: not-allowed;
+            }
+            &:not(:disabled):hover {
                 background: ${props => props.theme["green-700"]};
                 transition: background-color 0.2s;
             }
@@ -74,10 +79,10 @@ export const TransactionType = styled(RadioGroup.Root)`
     margin-top: 0.5rem;
 
 `
-interface TransactionTypeButtonProps{
+interface TransactionTypeButtonProps {
     variant: 'income' | 'outcome';
 }
-export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item) <TransactionTypeButtonProps>`
     background: ${props => props.theme["gray-700"]};
     padding: 1rem;
     display: flex;
